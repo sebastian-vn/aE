d8dd826769cd8499436e497105cab89cfa2c5537
sebastian-vn <sebastian.norena@umanizales.edu.co>
2018-08-29 17:41:36 -0500

    updates

--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
 $(function() {
   /* Bootstrap elements */
-  $('.carousel').carousel()
+  $(".carousel").carousel();
 
   getTemas();
+  var tema = getTemaURL("id_tema");
+
+  if (getTemaURL("id_tema") != "") {
+    getSubtemas(tema);
+  }
 
   /* ONCLICKS */
   $("#menu").click(function() {
@@ -12,13 +17,17 @@ $(function() {
   $(".closebtn").click(function() {
     closeSidenav();
   });
+
+  setTimeout(function(){
+    $(".preloader").fadeOut();
+  }, 1500);
 });
 
 function openSidenav() {
   $(".sidenav").addClass(
     "sidenav-open"
   ); /* Adds class s-open to move sidenav */
-  $("#main").addClass("main-move"); /* Main moves with sidenav */
+  //$("#main").addClass("main-move"); /* Main moves with sidenav */
 }
 
 function closeSidenav() {
@@ -30,6 +39,18 @@ function closeSidenav() {
   ); /* Removes class to set main to normal state */
 }
 
+function getTemaURL(variable) {
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=");
+    if (pair[0] == variable) {
+      return pair[1];
+    }
+  }
+  return false;
+}
+
 function getTemas() {
   $.ajax({
     type: "POST",
@@ -38,7 +59,7 @@ function getTemas() {
     success: function(response) {
       response.forEach(element => {
         $("#leftSideNav").append(
-          `<a href="#${element.id_tema}">${element.tema}</a>
+          `<a id="temas" href="?id_tema=${element.id_tema}">${element.tema}</a>
           <hr/>`
         );
       });
@@ -47,22 +68,25 @@ function getTemas() {
 }
 
 function getSubtemas(tema) {
-  $("#sub-main").html("");
-  $.ajax({
-    type: "POST",
-    url: "server/getSubtemas.php",
-    dataType: "json",
-    data: {
-      tema : tema
-    },
-    success: function(response) {
-      response.forEach(element => {
-        $("#sub-main").append(
-          `<div class="alert alert-primary" role="alert">
-            <a href="#${element.id_subtema}">${element.subtema}</a>
+  $("#carousel").addClass("none");
+  $.ajax(
+    {
+      type: "POST",
+      url: "server/getSubtemas.php",
+      dataType: "json",
+      data: {
+        tema: tema
+      },
+      success: function(response) {
+        response.forEach(element => {
+          $("#sub-main").append(
+            `<div class="alert alert-primary" role="alert">
+            <a href="&id_subtema=${element.id_subtema}">${element.subtema}</a>
           </div>`
-        );
-      });
-    }
-  });
+          );
+        });
+      }
+    },
+    $("#sub-main").removeClass("none")
+  );
 }
